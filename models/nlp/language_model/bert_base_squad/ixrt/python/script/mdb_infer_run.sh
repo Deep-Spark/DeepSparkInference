54da9b92901a57252e10541af318c28b84a12d7b
majorli <mingjiang.li@iluvatar.com>
2024-07-30 16:15:02 +0800

    add bs input params
    Signed-off-by: majorli <mingjiang.li@iluvatar.com>

--- models/nlp/language_model/bert_base_squad/ixrt/python/script/mdb_infer_run.sh
+++ models/nlp/language_model/bert_base_squad/ixrt/python/script/mdb_infer_run.sh
@@ -15,21 +15,22 @@
 #    under the License.
 
 index=0
-options=("$@")  # 将所有参数存储到数组中
+options=("$@") # 将所有参数存储到数组中
 PRECISION=fp16
+BSZ=32
 
 # 循环遍历所有参数
 while [[ $index -lt ${#options[@]} ]]; do
     argument=${options[$index]}
     case $argument in
-        --bs)
-            ((index++))
-            BSZ=${options[$index]}
-            ;;
-        --prec)
-            ((index++))
-            PRECISION=${options[$index]}
-            ;;
+    --bs)
+        ((index++))
+        BSZ=${options[$index]}
+        ;;
+    --prec)
+        ((index++))
+        PRECISION=${options[$index]}
+        ;;
     esac
     ((index++))
 done
@@ -39,20 +40,33 @@ INT8_FLAG=""
 if [[ "$PRECISION" == "int8" ]]; then
     INT8_FLAG="--int8"
 fi
+
+# 设置BSZ_FLAG
+BSZ_FLAG=""
+if [[ "$BSZ" -ne 32 ]]; then
+    BSZ_FLAG="--bs $BSZ"
+fi
+
 echo "PREC_FLAG=$INT8_FLAG"
 echo "PRECISION=$PRECISION"
+echo "BSZ=$BSZ"
+echo "BSZ_FLAG=$BSZ_FLAG"
 
 # 检查环境并执行相应的脚本
 if command -v ixsmi &>/dev/null; then
     echo "MR env"
+    cmake -S . -B build
+    cmake --build build -j16
     cd ./python/script/
-    bash infer_bert_base_squad_${PRECISION}_ixrt.sh
+    bash infer_bert_base_squad_${PRECISION}_ixrt.sh $BSZ_FLAG
 
 elif command -v nvidia-smi &>/dev/null; then
     echo "NV env"
+    cmake -S . -B build -DUSE_TENSORRT=true
+    cmake --build build -j16
     cd ./python/
-    bash script/build_engine.sh --bs 32 $INT8_FLAG
-    bash script/inference_squad.sh --bs 32 $INT8_FLAG
+    bash script/build_engine.sh --bs $BSZ $INT8_FLAG
+    bash script/inference_squad.sh --bs $BSZ $INT8_FLAG
 else
     echo "No driver detected"
     exit 1
