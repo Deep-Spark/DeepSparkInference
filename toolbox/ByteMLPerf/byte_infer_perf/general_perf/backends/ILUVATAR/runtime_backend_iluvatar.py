25df3b4f9bea409fa6d84fb6a63285866f0dbcbf
majorli <mingjiang.li@iluvatar.com>
2024-08-05 15:23:13 +0800

    update copyrights
    Signed-off-by: majorli <mingjiang.li@iluvatar.com>

--- toolbox/ByteMLPerf/byte_infer_perf/general_perf/backends/ILUVATAR/runtime_backend_iluvatar.py
+++ toolbox/ByteMLPerf/byte_infer_perf/general_perf/backends/ILUVATAR/runtime_backend_iluvatar.py
@@ -1,4 +1,6 @@
 # Copyright 2023 Graphcore Ltd.
+# Copyright (c) 2024, Shanghai Iluvatar CoreX Semiconductor Co., Ltd.
+# All Rights Reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
