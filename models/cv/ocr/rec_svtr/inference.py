c88f56a5f05cdeb2ffe951c913dce462384c342c
YoungPeng <peng.yang@iluvatar.com>
2024-10-12 15:45:48 +0800

    Add: rec_svtr inference script.

---
+++ models/cv/ocr/rec_svtr/inference.py
@@ -0,0 +1,192 @@
+# Copyright (c) 2024, Shanghai Iluvatar CoreX Semiconductor Co., Ltd.
+# All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+import os
+import argparse
+import tvm
+import yaml
+import logging
+import numpy as np
+from tvm import relay
+from tqdm import tqdm
+
+import paddle
+from paddleocr import ppocr
+from ppocr.data import build_dataloader
+from ppocr.utils.logging import get_logger
+from ppocr.postprocess import build_post_process
+
+import string
+from rapidfuzz.distance import Levenshtein
+
+
+class RecMetric(object):
+    def __init__(self,
+                 main_indicator='acc',
+                 is_filter=False,
+                 ignore_space=True,
+                 **kwargs):
+        self.main_indicator = main_indicator
+        self.is_filter = is_filter
+        self.ignore_space = ignore_space
+        self.eps = 1e-5
+        self.reset()
+
+    def _normalize_text(self, text):
+        text = ''.join(
+            filter(lambda x: x in (string.digits + string.ascii_letters), text))
+        return text.lower()
+
+    def __call__(self, pred_label, *args, **kwargs):
+        preds, labels = pred_label
+        correct_num = 0
+        all_num = 0
+        norm_edit_dis = 0.0
+        for (pred, pred_conf), (target, _) in zip(preds, labels):
+            if self.ignore_space:
+                pred = pred.replace(" ", "")
+                target = target.replace(" ", "")
+            if self.is_filter:
+                pred = self._normalize_text(pred)
+                target = self._normalize_text(target)
+            norm_edit_dis += Levenshtein.normalized_distance(pred, target)
+            if pred == target:
+                correct_num += 1
+            all_num += 1
+        self.correct_num += correct_num
+        self.all_num += all_num
+        self.norm_edit_dis += norm_edit_dis
+        return {
+            'acc': correct_num / (all_num + self.eps),
+            'norm_edit_dis': 1 - norm_edit_dis / (all_num + self.eps)
+        }
+
+    def get_metric(self):
+        """
+        return metrics {
+                 'acc': 0,
+                 'norm_edit_dis': 0,
+            }
+        """
+        acc = 1.0 * self.correct_num / (self.all_num + self.eps)
+        norm_edit_dis = 1 - self.norm_edit_dis / (self.all_num + self.eps)
+        self.reset()
+        return {'acc': acc, 'norm_edit_dis': norm_edit_dis}
+
+    def reset(self):
+        self.correct_num = 0
+        self.all_num = 0
+        self.norm_edit_dis = 0
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    
+    parser.add_argument("--engine", 
+                        type=str, 
+                        required=True, 
+                        help="igie engine path.")
+    
+    parser.add_argument("--batchsize",
+                        type=int,
+                        required=True, 
+                        help="inference batch size.")
+    
+    parser.add_argument("--datasets", 
+                        type=str, 
+                        required=True, 
+                        help="datasets path.")
+
+    parser.add_argument("--input_name", 
+                        type=str, 
+                        required=True, 
+                        help="input name of the model.")
+    
+    parser.add_argument("--warmup", 
+                        type=int, 
+                        default=3, 
+                        help="number of warmup before test.")           
+
+    parser.add_argument("--perf_only",
+                        type=bool,
+                        default=False,
+                        help="Run performance test only")
+    
+    args = parser.parse_args()
+
+    return args
+
+def main():
+    args = parse_args()
+
+    logger = get_logger(log_level=logging.INFO)
+    
+    config = yaml.load(open("rec_svtr_tiny_6local_6global_stn_en.yml", 'rb'), Loader=yaml.Loader)
+    config['Eval']['loader']['batch_size_per_card'] = args.batchsize
+    config['Eval']['dataset']['data_dir'] = os.path.join(args.datasets)
+
+    # build dataloader
+    config['Eval']['loader']['drop_last'] = True
+    valid_dataloder = build_dataloader(config, 'Eval', paddle.set_device("cpu"), logger)
+
+    # build post process
+    global_config = config['Global']
+    post_process_class = build_post_process(config['PostProcess'], global_config)
+
+    # build metric
+    eval_class = eval('RecMetric')()
+
+    # creat target and device
+    target = tvm.target.iluvatar(model="MR", options="-libs=cudnn,cublas,ixinfer")    
+    device = tvm.device(target.kind.name, 0)
+
+    # load engine
+    lib = tvm.runtime.load_module(args.engine)
+
+    # create runtime from engine
+    module = tvm.contrib.graph_executor.GraphModule(lib["default"](device))
+    
+    # just run perf test
+    if args.perf_only:
+        ftimer = module.module.time_evaluator("run", device, number=100, repeat=1)        
+        prof_res = np.array(ftimer().results) * 1000 
+        fps = args.batchsize * 1000 / np.mean(prof_res)
+        print(f"\n* Mean inference time: {np.mean(prof_res):.3f} ms, Mean fps: {fps:.3f}")
+    else:
+        # warm up
+        for _ in range(args.warmup):
+            module.run()
+
+        for batch in tqdm(valid_dataloder):
+            images = batch[0]
+
+            module.set_input(args.input_name, tvm.nd.array(images, device))
+
+            module.run()
+
+            outputs = module.get_output(0).asnumpy()
+            outputs = paddle.to_tensor(outputs)
+
+            batch_numpy = []
+            for item in batch:
+                batch_numpy.append(item.numpy())
+            
+            post_result = post_process_class((outputs), batch_numpy[1])
+            eval_class(post_result, batch_numpy)
+
+        metric = eval_class.get_metric()
+        print(metric)
+
+if __name__ == "__main__":
+    main()
