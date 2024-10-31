4549f5d1238cb5a452c3d59cb2b7ba18bd5421a9
hongliang.yuan <hongliang.yuan@iluvatar.com>
2024-10-31 15:52:55 +0800

    fix efficentent v2 prepare.sh

--- models/cv/classification/efficientnet_v2/ixrt/ci/prepare.sh
+++ models/cv/classification/efficientnet_v2/ixrt/ci/prepare.sh
@@ -6,5 +6,5 @@ rm pytorch-image-models/timm/models/_builder.py
 mv ./_builder.py pytorch-image-models/timm/models
 mkdir -p /root/.cache/torch/hub/checkpoints/
 ln -s /root/data/checkpoints/efficientnet_v2.pth /root/.cache/torch/hub/checkpoints/efficientnetv2_t_agc-3620981a.pth
-python3 -m pytorch-image-models/timm/models.export_onnx --output_model ./checkpoints/efficientnet_v2.onnx
+cd pytorch-image-models/timm/ && python3 -m models.export_onnx --output_model ../../checkpoints/efficientnet_v2.onnx
 export PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION=python
