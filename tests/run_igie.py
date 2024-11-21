b86d87245c214796ed47c64bbef9930731aa87c6
mingjiang.li <mingjiang.li@iluvatar.com>
2024-11-21 14:33:13 +0800

    add classification codes
    Signed-off-by: mingjiang.li <mingjiang.li@iluvatar.com>

--- tests/run_igie.py
+++ tests/run_igie.py
@@ -18,6 +18,14 @@ import subprocess
 import json
 import re
 import time
+import logging
+
+# 配置日志
+logging.basicConfig(
+    filename="output.log",  # 日志文件名
+    level=logging.INFO,     # 日志级别
+    format="%(asctime)s - %(message)s"  # 日志格式
+)
 
 def main():
     with open("models_igie.yaml", "r") as file:
@@ -31,7 +39,7 @@ def main():
                 'Top5 acc':'79.055 %',
                 'Mean inference time': '1.476 ms',
                 'Mean fps': '21685.350'
-                
+
             },
             'int8': {
                 'Top1 acc':'55.538 %',
@@ -43,79 +51,69 @@ def main():
         'status': 'fail'
     }
 
+    # 分类模型
     for index, model in enumerate(models):
-        if model["task_type"] == "cv/classification" and model['name']=="alexnet":
-            print(f"{index}, {model['name']}")
+        avail_models = ["densenet161", "densenet169", "densenet201", "efficientnetv2_rw_t", "efficientnet_b0", "efficientnet_b1", "efficientnet_b2", "efficientnet_b3", "efficientnet_v2", "efficientnet_v2_s", "googlenet", "hrnet_w18", "inception_v3", "mnasnet0_5", "mobilenet_v2", "mobilenet_v3", "mobilenet_v3_large", "mvitv2_base", "regnet_x_1_6gf", "regnet_y_1_6gf", "repvgg", "res2net50", "resnest50", "resnet101", "resnet152", "resnet18", "resnet50", "resnetv1d50", "resnext101_64x4d", "resnext50_32x4d", "se_resnet50", "shufflenetv2_x0_5", "shufflenetv2_x1_0", "shufflenetv2_x1_5", "vgg16", "wide_resnet50"
+]
+        if model["task_type"] == "cv/classification" and model['name'] in avail_models:
+            logging.info(f"{index}, {model['name']}")
+            logging.info(json.dumps(model, indent=4))
             d_url = model['download_url']
             test_data = []
             if d_url is not None and (d_url.endswith('.pth') or d_url.endswith('.pt')):
                 cur_result = {'name': model['name'], 'result':'', }
                 checkpoint_n = d_url.split("/")[-1]
-                # 执行后获取结果
-                script = f"""
-                pwd
-                pip3 install onnx tqdm
-                # ln -s /mnt/deepspark/data/checkpoints/igie/{checkpoint_n} ./
-                python3 export.py --weight /mnt/deepspark/data/checkpoints/igie/{checkpoint_n} --output alexnet.onnx
 
-                export DATASETS_DIR=/mnt/deepspark/volumes/mdb/data/datasets/imagenet-val
-                cd ../{model['relative_path']}
-
-                # echo "****run fp16 acc ****"
-                # bash scripts/infer_alexnet_fp16_accuracy.sh
-                # echo "****run fp16 perf ****"
-                bash scripts/infer_alexnet_fp16_performance.sh
-
-                """
-
-                # result = subprocess.run(script, shell=True, capture_output=True,text=True,executable="/bin/bash")
-                # print("标准输出:", result.stdout)
-                # print("标准错误:", result.stderr)
-                # print("返回码:", result.returncode)
-                # if result.returncode ==0:
-                #     test_data.append(cur_result)
                 test_data.append(run_clf_testcase(model))
-                
-            print(json.dumps(test_data, indent=4))
-        
+
+            logging.info(json.dumps(test_data, indent=4))
+
+        # 检测模型
+
 def run_clf_testcase(model):
-    result = {'name': model['name'], 'result':{}, }
+    model_name = model['name']
+    result = {'name': model_name, 'result':{}, }
     d_url = model['download_url']
     checkpoint_n = d_url.split("/")[-1]
     prepare_script = f"""
     pip3 install onnx tqdm
     # ln -s /mnt/deepspark/data/checkpoints/igie/{checkpoint_n} ./
-    python3 export.py --weight /mnt/deepspark/data/checkpoints/igie/{checkpoint_n} --output alexnet.onnx
+    cd ../{model['relative_path']}
+    python3 export.py --weight /mnt/deepspark/data/checkpoints/igie/{checkpoint_n} --output {model_name}.onnx
+    ls -l | grep onnx
     """
     run_script(prepare_script)
 
-    # for prec in model['precisions']:
-    for prec in ['fp16']:
-        print(f"run {prec} test case")
+    for prec in model['precisions']:
+        logging.info(f"run {prec} test case")
         script = f"""
         export DATASETS_DIR=/mnt/deepspark/volumes/mdb/data/datasets/imagenet-val
         cd ../{model['relative_path']}
-        bash scripts/infer_alexnet_{prec}_accuracy.sh
-        bash scripts/infer_alexnet_{prec}_performance.sh
+        bash scripts/infer_{model_name}_{prec}_accuracy.sh
+        bash scripts/infer_{model_name}_{prec}_performance.sh
         """
-        
+
         r = run_script(script)
         sout = r.stdout
-        print("标准输出:", r.stdout)
-        print("标准错误:", r.stderr)
-        print("返回码:", r.returncode)
-        pattern = r"\* ([\w\d ]+):\s*([\d.]+ [ms%]+)\s*, ([\w\d ]+):\s*([\d.]+ [ms%]+)"
+        logging.info("标准输出:", r.stdout)
+        logging.info("标准错误:", r.stderr)
+        logging.info("返回码:", r.returncode)
+        pattern = r"\* ([\w\d ]+):\s*([\d.]+)[ ms%]*, ([\w\d ]+):\s*([\d.]+)[ ms%]*"
         # match = re.search(pattern, sout)
         # if match:
         #     result['result'][prec]={match.group(1):match.group(2), match.group(3):match.group(4)}
         matchs = re.findall(pattern, sout)
         for m in matchs:
-            result['result'][prec]={m[0]:m[1],m[2]:m[3]}
-
-        print(matchs)
+            # if not result['result'][prec]:
+            #     result['result'][prec] = {}
+            result['result'].setdefault(prec, {})
+            result['result'][prec]=result['result'][prec] | {m[0]:m[1],m[2]:m[3]}
+        logging.info("**************")
+        logging.info(matchs)
+        logging.info("**************")
 
     return result
-    
+
 
 def run_perf_script(script):
     return run_script(script)
@@ -125,8 +123,9 @@ def run_script(script):
     result = subprocess.run(script, shell=True, capture_output=True,text=True,executable="/bin/bash")
     end_time = time.perf_counter()
     execution_time = end_time - start_time
-    print("执行时间: {:.4f} 秒".format(execution_time))
+    logging.info(f"执行命令：\n{script}")
+    logging.info("执行时间: {:.4f} 秒".format(execution_time))
     return result
-    
+
 if __name__ == "__main__":
     main()
