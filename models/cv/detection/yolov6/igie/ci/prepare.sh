a3e70d1adb2e9044035bcb34ebc621b7ba6c2b48
majorli6 <mingjiang.li@iluvatar.com>
2024-12-25 05:45:44 +0000

    update models/cv/detection/yolov6/igie/ci/prepare.sh.
    Signed-off-by: majorli6 <mingjiang.li@iluvatar.com>

--- models/cv/detection/yolov6/igie/ci/prepare.sh
+++ models/cv/detection/yolov6/igie/ci/prepare.sh
@@ -28,7 +28,7 @@ fi
 pip3 install -r requirements.txt
 
 # install yolov6
-git clone --depth 1 https://github.com/meituan/YOLOv6.git
+git clone --depth 1 https://gitee.com/monkeycc/YOLOv6
 cd YOLOv6
 pip3 install -r requirements.txt
 
