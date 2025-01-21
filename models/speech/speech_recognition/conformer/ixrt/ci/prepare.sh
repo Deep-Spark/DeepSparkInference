9e6138173e7934fec928d845131c4cc9bc5ad3d3
hongliang.yuan <hongliang.yuan@iluvatar.com>
2025-01-21 14:14:33 +0800

    update

--- models/speech/speech_recognition/conformer/ixrt/ci/prepare.sh
+++ models/speech/speech_recognition/conformer/ixrt/ci/prepare.sh
@@ -29,5 +29,5 @@ pip3 install -r requirements.txt
 
 ln -s /root/data/checkpoints/20210601_u2++_conformer_exp_aishell ./conformer_checkpoints
 
-cp -r /root/data/datasets/AISHELL/data_aishell ./aishell_test_data
-bash scripts/aishell_data_prepare.sh aishell_test_data tools
+ln -s /root/data/datasets/AISHELL/data_aishell ./aishell_test_data
+bash scripts/aishell_data_prepare.sh ./aishell_test_data ./tools
