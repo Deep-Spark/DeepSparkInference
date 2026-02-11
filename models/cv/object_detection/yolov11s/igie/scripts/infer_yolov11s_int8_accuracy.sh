95ece64c30d585204a3d2852f318bc1cfaf0f3e1
YoungPeng <peng.yang@iluvatar.com>
2026-02-11 10:32:07 +0800

    Add: yolov11s inference script.

---
+++ models/cv/object_detection/yolov11s/igie/scripts/infer_yolov11s_int8_accuracy.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+# Copyright (c) 2025, Shanghai Iluvatar CoreX Semiconductor Co., Ltd.
+# All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+batchsize=32
+model_path="yolo11s.onnx"
+quantized_model_path="yolo11s_int8.onnx"
+datasets_path=${DATASETS_DIR}
+
+# Update arguments
+index=0
+options=$@
+arguments=($options)
+for argument in $options
+do
+    index=`expr $index + 1`
+    case $argument in
+      --bs) batchsize=${arguments[index]};;
+    esac
+done
+
+echo "batch size is ${batchsize}"
+
+if [ ! -e $quantized_model_path ]; then
+    # quantize model to int8
+    python3 quantize.py                       \
+        --model_path ${model_path}            \
+        --out_path ${quantized_model_path}    \
+        --batch ${batchsize}                  \
+        --datasets ${datasets_path}
+fi
+
+# build engine
+python3 ../../igie_common/build_engine.py    \
+    --model_path ${quantized_model_path}     \
+    --input images:${batchsize},3,640,640    \
+    --precision int8                         \
+    --engine_path yolo11s_bs_${batchsize}_int8.so
+
+# inference
+python3 inference.py                              \
+    --engine yolo11s_bs_${batchsize}_int8.so      \
+    --batchsize ${batchsize}                      \
+    --input_name images                           \
+    --datasets ${datasets_path}
